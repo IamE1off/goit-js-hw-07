e7f27b32be9cd81b8476d42d19898e853d2c1736
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-04-14 23:15:31 +0200

    task 6
    not finished styling input and background

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
+"use strict";
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxDiv = document.querySelector("#boxes");
+
+function onCreateBoxes(amount) {
+  boxDiv.innerHTML = "";
+  const arrayDiv = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    arrayDiv.push(div);
+    size += 10;
+  }
+  boxDiv.append(...arrayDiv);
+}
+
+function onDestroyBoxes() {
+  boxDiv.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    onCreateBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", onDestroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
