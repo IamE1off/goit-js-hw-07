7802f0f61a47c4b99d478ba88b14c2dfdee10863
IamE1off <132561973+IamE1off@users.noreply.github.com>
2024-04-15 21:45:33 +0200

    Update task-4.js
    trim input data

--- js/task-4.js
+++ js/task-4.js
@@ -5,13 +5,13 @@ loginForm.addEventListener("submit", function (event) {
   event.preventDefault(); 
   const emailInput = this.elements.email;
   const passwordInput = this.elements.password;
-  if (!emailInput.value || !passwordInput.value) {
+  if (!emailInput.value.trim() || !passwordInput.value.trim()) {
     alert("Всі поля повинні бути заповнені!");
     return;
   }
   const formData = {
-    email: emailInput.value,
-    password: passwordInput.value,
+    email: emailInput.value.trim(),
+    password: passwordInput.value.trim(),
   };
   console.log(formData);
   this.reset();
